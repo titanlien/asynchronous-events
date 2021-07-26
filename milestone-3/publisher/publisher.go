1170e91969f845565ff04cfdd9e516bd5b5e4d8f
MH Lien <titan@mhlien.tw>
2021-07-26 11:36:03 +0200

    Init milestones-3

---
+++ milestone-3/publisher/publisher.go
@@ -0,0 +1,54 @@
+package publisher
+
+import (
+	"encoding/json"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+
+	"github.com/titanlien/asynchronous-events/milestone-3/config"
+	"github.com/titanlien/asynchronous-events/milestone-3/events"
+)
+
+// PublishEvent will publish the specified event to the messaging system (currently running on localhost)
+func PublishEvent(event events.Event, topic string) error {
+
+	log.WithField("event", event).Info("attempting to publish event")
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers":   config.BrokerAddress(),
+		"socket.timeout.ms":   30000,
+		"delivery.timeout.ms": 30000})
+	if err != nil {
+		return err
+	}
+
+	// Optional delivery channel, if not specified the Producer object's
+	// .Events channel is used.
+	deliveryChan := make(chan kafka.Event)
+
+	var value []byte
+	if value, err = json.Marshal(event); err != nil {
+		return err
+	}
+	err = p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          value,
+	}, deliveryChan)
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		return m.TopicPartition.Error
+	}
+
+	log.WithField("Name", *m.TopicPartition.Topic).
+		WithField("Partition", m.TopicPartition.Partition).
+		WithField("PartitionOffset", m.TopicPartition.Offset).
+		Infof("Delivered message to topic")
+
+	close(deliveryChan)
+
+	return nil
+}
